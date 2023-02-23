ea17f85f32645120c6da50dbe9190577a48664f5
Rimoosoo <50680955+Rimoosoo@users.noreply.github.com>
2023-02-23 13:48:48 +0900

    commit

--- src/ui/HomeScreen.js
+++ src/ui/HomeScreen.js
@@ -1,8 +1,16 @@
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
-import { Pressable, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React, { useCallback, useState } from "react";
+import {
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+} from "react-native";
 import MaterialIcon from "react-native-vector-icons/MaterialCommunityIcons";
 import Colors from "../../assets/Colors";
+import * as SplashScreen from "expo-splash-screen";
 
 const brands = [
   {
@@ -36,8 +44,19 @@ const brands = [
 function HomeScreen() {
   const navigator = useNavigation();
 
+  const onLayoutRootView = useCallback(async () => {
+    if (menuImage) {
+      await SplashScreen.hideAsync();
+    }
+  }, [menuImage]);
+
+  if (!menuImage) {
+    // font load error
+    return null;
+  }
+
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={{ flex: 1 }} onLayoutRootView={onLayoutRootView}>
       <View style={styles.zeroContainer}>
         <Pressable
           style={{
@@ -49,41 +68,81 @@ function HomeScreen() {
             navigator.navigate("DeliveryScreen");
           }}
         >
-          <MaterialIcon name="home-edit" size={50} color="black" />
+          <MaterialIcon name="home-edit" size={50} color="white" />
         </Pressable>
-        <Text style={{ fontFamily: "gowun", fontSize: 20, paddingLeft: 10 }}>
+        <Text
+          style={{
+            fontFamily: "gowun",
+            fontSize: 20,
+            paddingLeft: 10,
+            color: "white",
+          }}
+        >
           서울 양천구 신정3동
         </Text>
       </View>
-      <View style={styles.firstContainer}>
-        <Pressable
-          style={styles.itemContainer}
-          onPress={() => {
-            navigator.navigate("StoreIntroduce", { brand: brands[0] });
-          }}
-        >
-          <Text style={styles.normalText}>우나기 맨숀</Text>
-        </Pressable>
-      </View>
-      <View style={styles.secondContainer}>
-        <Pressable
-          style={styles.itemContainer}
-          onPress={() => {
-            navigator.navigate("StoreIntroduce", { brand: brands[1] });
-          }}
-        >
-          <Text style={styles.normalText}>대창을 덮다</Text>
-        </Pressable>
-      </View>
-      <View style={styles.thirdContainer}>
-        <Pressable
-          style={styles.itemContainer}
-          onPress={() => {
-            navigator.navigate("StoreIntroduce", { brand: brands[2] });
-          }}
-        >
-          <Text style={styles.normalText}>뚝도 양조장</Text>
-        </Pressable>
+      <View style={styles.container}>
+        <View style={[styles.firstContainer, {}]}>
+          <Pressable
+            style={styles.itemContainer}
+            onPress={() => {
+              navigator.navigate("StoreIntroduce", { brand: brands[0] });
+            }}
+          >
+            <ImageBackground
+              source={require("../../assets/image/wood.jpg")}
+              style={{
+                width: "100%",
+                height: "100%",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Text style={styles.normalText}>우나기 맨숀</Text>
+            </ImageBackground>
+          </Pressable>
+        </View>
+        <View style={styles.secondContainer}>
+          <Pressable
+            style={styles.itemContainer}
+            onPress={() => {
+              navigator.navigate("StoreIntroduce", { brand: brands[1] });
+            }}
+          >
+            <ImageBackground
+              source={require("../../assets/image/wood.jpg")}
+              style={{
+                width: "100%",
+                height: "100%",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Text style={[styles.normalText]}>대창을 덮다</Text>
+            </ImageBackground>
+          </Pressable>
+        </View>
+
+        <View style={styles.thirdContainer}>
+          <Pressable
+            style={styles.itemContainer}
+            onPress={() => {
+              navigator.navigate("StoreIntroduce", { brand: brands[2] });
+            }}
+          >
+            <ImageBackground
+              source={require("../../assets/image/wood.jpg")}
+              style={{
+                width: "100%",
+                height: "100%",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Text style={styles.normalText}>뚝도 양조장</Text>
+            </ImageBackground>
+          </Pressable>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -93,7 +152,9 @@ export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 7,
+    backgroundColor: "black",
+    flexDirection: "row",
   },
   zeroContainer: {
     flex: 1,
@@ -125,11 +186,12 @@ const styles = StyleSheet.create({
     height: "50%",
     backgroundColor: Colors.componentColor.viewBox,
     borderRadius: 30,
+    overflow: "hidden",
   },
   //
   //
   normalText: {
-    color: "black",
+    color: "white",
     fontSize: 40,
     fontFamily: "gowun",
   },
